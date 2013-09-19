8206dd8687d241a3f47a9f92d310f0d244c2e888
Jearvon Dharrie <j.dharrie@gmail.com>
2013-09-19 01:06:06 -0400

    Basic hello world

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject shoutr "0.1.0-SNAPSHOT"
+  :description "Twitter like clone"
+  :url "http://github.com/iamjarvo/shoutr"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [ring/ring-jetty-adapter "1.1.6"]
+                 [compojure "1.1.3"]]
+  :main shoutr.core)
---
+++ test/shoutr/core_test.clj
@@ -0,0 +1,7 @@
+(ns shoutr.core-test
+  (:require [clojure.test :refer :all]
+            [shoutr.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
